b53587c021a56698582ee5c6cb6c09ef49973d8b
taoqf <tao_qiufeng@126.com>
2020-04-02 18:24:51 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-
-export default function awx6(param1: string, param2: string) {
+export default function add_phone_contact(options: wx.PhoneContact) {
+	return new Promise<boolean>((resolve, reject) => {
+		wx.addPhoneContact({
+			...options,
+			success() {
+				resolve(true);	// 需要判断一下返回值，用户取消|调用失败
+			},
+			fail() {
+				reject(false);
+			}
+		});
+	});
 }
